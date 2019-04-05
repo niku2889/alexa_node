9fc32d1dab4cc7cbddf9f1bd0f21cd23eaddb658
Nikunj Sathwara <nikunjsathwara039@gmail.com>
2019-04-05 20:51:42 +0530

    first

---
+++ index.js
@@ -0,0 +1,122 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const Alexa = require('ask-sdk-core');
+
+
+app.post('/', (req, res) => {
+
+    const LaunchRequestHandler = {
+        canHandle(handlerInput) {
+            return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+        },
+        handle(handlerInput) {
+            const speechText = 'Welcome to the compare the car part dot com, please tell us your vehicle registration number!';
+
+            return handlerInput.responseBuilder
+                .speak(speechText)
+                .reprompt(speechText)
+                .withSimpleCard('Hello World', speechText)
+                .getResponse();
+        },
+    };
+
+    const HelloWorldIntentHandler = {
+        canHandle(handlerInput) {
+            return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+                && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+        },
+        handle(handlerInput) {
+            const speechText = 'Hello World!';
+
+            return handlerInput.responseBuilder
+                .speak(speechText)
+                .withSimpleCard('Hello World', speechText)
+                .getResponse();
+        },
+    };
+
+    const HelpIntentHandler = {
+        canHandle(handlerInput) {
+            return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+                && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+        },
+        handle(handlerInput) {
+            const speechText = 'You can say hello to me!';
+
+            return handlerInput.responseBuilder
+                .speak(speechText)
+                .reprompt(speechText)
+                .withSimpleCard('Hello World', speechText)
+                .getResponse();
+        },
+    };
+
+    const CancelAndStopIntentHandler = {
+        canHandle(handlerInput) {
+            return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+                && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+                    || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+        },
+        handle(handlerInput) {
+            const speechText = 'Goodbye!';
+
+            return handlerInput.responseBuilder
+                .speak(speechText)
+                .withSimpleCard('Hello World', speechText)
+                .getResponse();
+        },
+    };
+
+    const SessionEndedRequestHandler = {
+        canHandle(handlerInput) {
+            return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+        },
+        handle(handlerInput) {
+            console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+            return handlerInput.responseBuilder.getResponse();
+        },
+    };
+
+    const ErrorHandler = {
+        canHandle() {
+            return true;
+        },
+        handle(handlerInput, error) {
+            console.log(`Error handled: ${error.message}`);
+
+            return handlerInput.responseBuilder
+                .speak('Sorry, I can\'t understand the command. Please say again.')
+                .reprompt('Sorry, I can\'t understand the command. Please say again.')
+                .getResponse();
+        },
+    };
+
+    const skillBuilder = Alexa.SkillBuilders.custom();
+
+    exports.handler = skillBuilder
+        .addRequestHandlers(
+            LaunchRequestHandler,
+            HelloWorldIntentHandler,
+            HelpIntentHandler,
+            CancelAndStopIntentHandler,
+            SessionEndedRequestHandler
+        )
+        .addErrorHandlers(ErrorHandler)
+        .lambda();
+
+    res.send({
+        version: '1.0',
+        response: {
+            shouldEndSession: false,
+            outputSpeech: {
+                type: 'SSML',
+                text: 'Hello World!',
+                ssml: '<speak>Hello World!</speak>'
+            }
+        }
+    })
+});
+
+app.listen(8080, () => console.log('Example app listening on port 8080!'));
