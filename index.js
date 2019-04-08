3035ce890dfd199b77bd4b2ac7a78f77a14b6759
Nikunj Sathwara <nikunjsathwara039@gmail.com>
2019-04-08 15:44:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Alexa = require('ask-sdk-core');
 
 
 app.post('/', (req, res) => {
-    console.log(req.event)
+    console.log(req)
     const LaunchRequestHandler = {
         canHandle(handlerInput) {
             return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
